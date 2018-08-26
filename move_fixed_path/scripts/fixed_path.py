7feb21448e44a854a0f8c65ae51ccfbd72c98b77
Muraleekrishna Gopinathan <muraleekrishnagc@gmail.com>
2018-08-26 19:11:55 +1000

    Update code

--- move_fixed_path/scripts/fixed_path.py
+++ move_fixed_path/scripts/fixed_path.py
@@ -1,96 +1,114 @@
 #!/usr/bin/env python
 import rospy
-from create_node.msg import TurtlebotSensorState
 from geometry_msgs.msg import Twist
-import sys, select, termios, tty
+from nav_msgs.msg import Odometry
 
-DIRECTION_MAPPING = {
-    'j': 1, # left
-    'l': -1, # right
-}
+LINEAR_SPEED = 0.2
+ANGULAR_SPEED = 0.2
+		
+class RobotController():
+	def __init__(self):
+		self.move_cmd = Twist()
+		rospy.on_shutdown(self.on_shutdown)
+		self.navi_pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+		rospy.Subscriber('/odom', Odometry, self.odometry_callback)
+		self.last_t = 0
+		self.x = 0
+		self.y = 0
+		self.file = open('data.txt', 'w+')
 
-SPEED_MAPPING = {
-    'i': 1, # increase speed
-    'm': -1, # decrease speed
-}
+	def move_forward(self):
+		start = rospy.get_rostime().secs
+		distance = 0
+		while distance <= 2:
+			self.move_cmd.angular.x = 0
+			self.move_cmd.angular.y = 0
+			self.move_cmd.angular.z = 0
+			self.move_cmd.linear.x = LINEAR_SPEED
+			self.move_cmd.linear.y = 0
+			self.move_cmd.linear.z = 0
+			self.navi_pub.publish(self.move_cmd)
+			distance = LINEAR_SPEED * (rospy.get_rostime().secs - start)
 
-class Robot():
-    def __init__(self):
-        rospy.init_node('keyboard_teleop', anonymous=False)
-        rospy.on_shutdown(self.on_shutdown)
-        self.cmd_pub = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=5) # navigation message publisher
-        self.should_stop = False # a flag to represent if robot should stop
-        self.move_cmd = Twist() # new empty message
+	def move_semi_circle(self):
+		start = rospy.get_rostime().secs
+		distance = 0
+		while distance <= 3.1415:
+			self.move_cmd.linear.x = LINEAR_SPEED
+			self.move_cmd.linear.y = 0
+			self.move_cmd.linear.z = 0
+			self.move_cmd.angular.z = ANGULAR_SPEED
+			self.move_cmd.angular.y = 0
+			self.move_cmd.angular.x = 0
+			self.navi_pub.publish(self.move_cmd)
+			distance = LINEAR_SPEED * (rospy.get_rostime().secs - start)
+
+	def turn_left(self):
+		start = rospy.get_rostime().secs
+		distance = 0
+		while distance <= 1.6:
+			self.move_cmd.linear.x = 0
+			self.move_cmd.linear.y = 0
+			self.move_cmd.linear.z = 0
+			self.move_cmd.angular.z = ANGULAR_SPEED
+			self.move_cmd.angular.y = 0
+			self.move_cmd.angular.x = 0
+			self.navi_pub.publish(self.move_cmd)
+			distance = ANGULAR_SPEED * (rospy.get_rostime().secs - start)
+
+	def stop(self):
+		self.move_cmd = Twist()
+		self.navi_pub.publish(self.move_cmd)
+
+	def send_data(self):
+		odometry.header.stamp = rospy.get_rostime()
+		odometry.header.frame_id = "odom"
+		odometry.child_frame_id = "base_link"
+		odometry.pose.pose.position.x = self.x
+		odometry.pose.pose.position.y = self.y
+		odometry.pose.pose.position.z = 0.0
+		odom_pub.publish(odometry)
+
+	def odometry_callback(self, data):
+		odometry = Odometry()
+		if(self.last_t == 0):
+			self.last_t = get_time(data)
+			pass
+		current_t = get_time(data)
+		dt  = current_t - self.last_t
+		angular_vel = data.twist.twist.angular.z
+		self.theta = self.theta + (angular_vel * dt)
+		vx = data.twist.twist.linear.x
+		vy = data.twist.twist.linear.y
+
+		self.x = self.x + (vx * math.cos(self.theta) - vy * math.sin(self.theta)) * dt
+		self.y = self.y + (vx * math.cos(self.theta) + vy * math.sin(self.theta)) * dt
+		print(self.x, self.y, self.theta)
+		self.last_t = current_t
+		self.send_data()
+		self.save_data_to_file()
     
-    def on_shutdown(self):
-        rospy.loginfo("Shutting down")
-        self.cmd_pub.publish(Twist()) # stop robot 
-        rospy.sleep(1)
+	def save_data_to_file(self):
+		self.file.write()
 
-    def on_sensor_event(self, data): 
-        if(data.cliff_left or data.cliff_right or data.cliff_front_right or data.cliff_front_left or data.bumps_wheeldrops > 0):
-            print('********* CLiff or Bump *******************') # print warning
-            self.should_stop = True # should stop if bumped or about to be cliff-ed
-        else:
-            self.should_stop = False
+	def on_shutdown(self):
+		rospy.loginfo("Shutting down")
+		self.navi_pub.publish(Twist()) # stop robotController
+		rospy.sleep(1)
 
-    def turn(self, direction, speed): 
-        self.move_cmd.angular.z = direction * speed # just change direction and speed  of angular x 
-        self.move_cmd.angular.y = 0
-        self.move_cmd.angular.x = 0
-        return self
-
-    def move_forward(self, speed):
-        self.move_cmd.linear.x = speed # set current speed 
-        self.move_cmd.linear.y = 0
-        self.move_cmd.linear.z = 0
-        return self
-        
-    def stop(self):
-        self.move_cmd = Twist() # all values to zero
-        self.cmd_pub.publish(self.move_cmd)
-    
-    def do_it(self, time):
-        rate = rospy.Rate(time)
-        while(not self.should_stop):
-            self.cmd_pub.publish(self.move_cmd)
-            rate.sleep()
+def get_time(data):
+	return float(str(data.header.stamp.secs) + '.' + str(data.header.stamp.nsecs))
 
 if __name__ == '__main__':
-    robot = Robot() # create an instance of above Class
-    rate = rospy.Rate(1)
-    stop = False # flag to denote robot state
-    while not rospy.is_shutdown():
-        # if(robot.should_stop or stop): #stp if already stopped
-        #     stop = True
-
-        # if(stop):        
-        #     robot.stop()
-        # else:
-        robot.move_forward(0.2)
-        robot.do_it(10)
-        robot.stop()
-
-        robot.move_forward(0.1).turn(-1, 0.1)
-        robot.do_it(5)
-        robot.stop()
-
-        robot.move_forward(0.2)
-        robot.do_it(10)
-
-        robot.turn(-1, 0.2)
-        robot.do_it(5)
-        robot.stop()
-
-        robot.move_forward(0.2)
-        robot.do_it(10)
-        robot.stop()
-
-        robot.turn(-1, 0.2)
-        robot.do_it(5)
-        robot.stop()
-
-        rate.sleep()
-
-
-        
+	rospy.init_node('fixed_path', anonymous=False)
+	odom_pub = rospy.Publisher('~estimated_odom', Odometry, queue_size=10)
+	try:
+		robot_ctrl = RobotController()
+		robot_ctrl.move_forward()
+		robot_ctrl.move_semi_circle()
+		robot_ctrl.move_forward()
+		robot_ctrl.turn_left()
+		robot_ctrl.move_forward()
+		robot_ctrl.turn_left()
+		robot_ctrl.stop()
+	except rospy.ROSInterruptException: pass
